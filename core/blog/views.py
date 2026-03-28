5e89d6bb66f59322350dd5974928a80312771e53
Mrinall Samal <samalmrinall9@gmail.com>
2026-03-28 16:05:24 +0530

    complete ch-2

--- core/blog/views.py
+++ core/blog/views.py
@@ -1,9 +1,10 @@
 # blog/views.py
 from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_POST
 from .models import Post
 from django.views.generic import ListView
-from .forms import EmailPostForm
+from .forms import EmailPostForm,CommentForm
 from django.core.mail import send_mail
 
 class PostListView(ListView):
@@ -34,7 +35,9 @@ def post_details(request, year, month, day, post):
         publish__month=month,
         publish__day=day,
     )
-    return render(request, 'blog/post/details.html', {'post': post})
+    comments = post.comments.filter(active=True)
+    form = CommentForm()
+    return render(request, 'blog/post/details.html', {'post': post,'comments': comments,'form': form})
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
@@ -60,3 +63,12 @@ def post_share(request, post_id):
     else:
         form = EmailPostForm()
     return render(request, 'blog/post/share.html', {'post': post, 'form': form, 'sent': sent})
+@require_POST
+def post_comment(request, post_id):
+    post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        comment = form.save(commit=False)
+        comment.post = post
+        comment.save()
+    return render(request, 'blog/post/comment.html', {'post': post, 'form': form})
